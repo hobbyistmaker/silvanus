7dbdfc1b286d49ccfc00e644e79c761402218675
Hobbyist Maker <44534843+hobbyistmaker@users.noreply.github.com>
2020-08-11 18:22:26 -0400

    Prep for CPP version

--- lib/__init__.py
+++
@@ -1,4 +0,0 @@
-import logging
-from logging import NullHandler as _NullHandler
-
-logging.getLogger('silvanus.lib').addHandler(_NullHandler())
--- util/__init__.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-from logging import NullHandler as _NullHandler
-
-from .log import config_log
-from .appcontext import appcontext
-
-logging.getLogger('silvanus').getChild(__name__).addHandler(_NullHandler())
